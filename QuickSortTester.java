d5c6f198d38885e73b1a59591de27b54fd3dde63
kmatsuyoshi <kmatsuyoshi@stuy.edu>
2016-02-12 17:58:43 -0500

    TESTSgit status!

--- QuickSortTester.java
+++ QuickSortTester.java
@@ -5,6 +5,10 @@
 
 public class QuickSortTester{
 
+    public static int[] arr;
+    public static int size;
+    public static double totalTime;
+    
     public static int[] generateArray(int n) {
 	int[] output = new int[n];
 	for (int i = 0; i < n; i++) {
@@ -12,16 +16,44 @@ public class QuickSortTester{
 	}
 	return output;
     }
+
+    public static void printArray( int[] array ) {
+	System.out.print("[");
+	for( int i = 0; i < array.length - 1; i++ )
+	System.out.print( array[i] + ",");
+	System.out.println( array[(array.length - 1)] + "]");
+    }
     
+    public static void addTime() {
+	for(int i = 0; i < 10; i++ ){ // 10 trials
+	    arr = generateArray(size);
+	    double before = System.nanoTime();
+	    QuickSort.qsort(arr);
+	    totalTime += System.nanoTime()-before;
+	}
+    }
+
+    public static void write() {	// displays time
+	totalTime = 0;		
+	addTime();
+	double time = totalTime / 10;
+	System.out.println(size + "," + time + "," + ( time / size ));
+    }
+
     public static void main(String []args){
+	/*
 	int[] arr = generateArray(100);
+
 	//Warmup
 	for (int i = 0; i < 100; i++) {
 	    QuickSort.qsort(arr);
 	}
-    }
+	*/
+	System.out.println("size,time(nanoSeconds),timePerSize");
+	for( size = 1000; size <= 10000; size += 100 ){
+	    write();
+	}
+	
+    } // end of main method
 
-
-
-
-}
+} // end of class QuickSortTester
