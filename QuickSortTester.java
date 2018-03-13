8e233bf9e10e58287a56222889c749aa60f93ea8
victorlin7534 <vlin5@stuy.edu>
2018-03-13 08:24:37 -0400

    update QuickSortTester

---
+++ QuickSortTester.java
@@ -0,0 +1,17 @@
+//Zoom Zoom Zucchini - Kayli Matsuyoshi, Kevin Lin, Victor Lin
+//APCS2 --  pd1
+//Lab01 --  What Does the Data Say?
+//2018-03-14W
+
+public class QuickSortTester{
+
+    
+    
+    public static void main(String []args){
+	
+    }
+
+
+
+
+}
