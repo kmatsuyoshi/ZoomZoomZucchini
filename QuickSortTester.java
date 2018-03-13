dbd488c4e543b2c8d70b8fea7e4a5f8bfc915fd2
Stuy-Kevin-Lin <klin14@stuy.edu>
2018-03-13 07:35:49 -0400

    Update

--- QuickSortTester.java
+++ QuickSortTester.java
@@ -5,10 +5,20 @@
 
 public class QuickSortTester{
 
-    
+    public static int[] generateArray(int n) {
+	int[] output = new int[n];
+	for (int i = 0; i < n; i++) {
+	    output[i] = (int) (Math.random() * 10000);
+	}
+	return output;
+    }
     
     public static void main(String []args){
-	
+	int[] arr = generateArray(100);
+	//Warmup
+	for (int i = 0; i < 100; i++) {
+	    QuickSort.qsort(arr);
+	}
     }
 
 
